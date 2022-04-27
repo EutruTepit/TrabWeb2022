6193d41d9a6798202a0a4bb5a0488318583cdb6d
LucasAriatti <lucas.ariatti@gmail.com>
2022-04-27 16:05:50 -0300

    Ariatti
    Criado a migration e model de produto e fornecedor

---
+++ app/Models/produto.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class produto extends Model
+{
+    use HasFactory;
+    protected $table = "produto";
+
+    function fornecedor()
+    {
+        return $this->belongsTo(fornecedor::class, 'idFornecedor', 'id');
+    }
+}
