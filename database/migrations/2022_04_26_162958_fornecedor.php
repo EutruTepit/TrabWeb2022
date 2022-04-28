4f74bff6616546e442993f487e41b1a2d0462d17
Luís Felippe Tomazini Fernandes <37600658+EutruTepit@users.noreply.github.com>
2022-04-28 17:28:30 -0300

    Update 2022_04_26_162958_fornecedor.php
    Trocando dropSoftSelets n desfaz a criação da tabela. Uso do dropIfExists pra isso

--- database/migrations/2022_04_26_162958_fornecedor.php
+++ database/migrations/2022_04_26_162958_fornecedor.php
@@ -32,7 +32,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('fornecedor', function (Blueprint $table) {
-            $table->dropSoftDeletes();
+            Schema::dropIfExists('fornecedor');
         });
     }
 };
