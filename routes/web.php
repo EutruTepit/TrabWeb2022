31eb5abedf9b91d470a33dffd6771cdd9a3bf96e
Luís Felippe Tomazini Fernandes <37600658+EutruTepit@users.noreply.github.com>
2022-05-02 21:53:19 -0300

    Organizando as rotas

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+# Rotas que precisam de autenticação para entrar
+Route::middleware('auth')->group(function(){
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    # Agrupamento de rotas referentes ao admin
+    Route::middleware('verifica.admin')->group(function(){
+
+    });
+
+});
 
 require __DIR__.'/auth.php';
