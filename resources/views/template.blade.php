db2a96b95112af65030b990c5698ad798168eb0b
LucasAriatti <lucas.ariatti@gmail.com>
2022-05-16 18:50:10 -0300

    At019
    Criacao  da home e mudificacao da rota

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -73,14 +73,25 @@
       display: flex;
       margin-left: 10px;
     }
+
+    .aba_categorias{
+      background-color:;
+      height: 50%;
+      align-self: auto;
+    }
+
+    .categorias{
+
+    }
 </style>
     
 
 </head>
+
 <body>
-    
-<div class="head">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+<div class="headd-none .d-md-block .d-lg-none">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -119,20 +130,15 @@
   </div>
 </nav>
 </div>
-
 <div class="corpo">
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-2" ></div>
+            <div class="col-2" >@yield('categorias') </div>
             <div class="col-9">@yield('conteudo')</div>
             <div class="col-1"></div>
     </div>
 </div>
-
-<!------------------------------------------------------------------------------------------->
-
-
 </div>
 
 
@@ -178,8 +184,8 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
