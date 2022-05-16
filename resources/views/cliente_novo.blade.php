3de647c49c5683829a6ff18d51f05eb4f33c4665
Luís Felippe Tomazini Fernandes <37600658+EutruTepit@users.noreply.github.com>
2022-05-16 20:43:18 -0300

    tirando Tags desnecessarias

--- resources/views/cliente_novo.blade.php
+++ resources/views/cliente_novo.blade.php
@@ -1,42 +1,37 @@
-<head>
-    <link rel="stylesheet" type="text/css" href="C:\xampp\htdocs\TrabWeb2022\resources\views\template.blade.php" />
-</head>
-
-<body>
-    @extends('template')
+@extends('template')
 
 
-    @section('categoria')
+@section('categoria')
 
-        <div class="categoria">
-            Test
-        </div>
+    <div class="categoria">
+        Test
+    </div>
 
 
-    @endsection
-    @section('conteudo')
-        <div class="borda_superior">
-            <h1>Hello</h1>
-        </div>
+@endsection
+@section('conteudo')
+    <div class="borda_superior">
+        <h1>Hello</h1>
+    </div>
 
-    <h1>Novo cliente</h1>
-    <form method="post" action="{{ route('clientes_novo') }}">
-        @csrf
-        <label>Nome</label>
-        <input type="text" name="nome"><br>
-        <label>CPF</label>
-        <input type="text" name="cpf"><br>
-        <label>RG</label>
-        <input type="text" name="rg"><br>
-        <label>Data de Nascimento</label>
-        <input type="date" name="data_nasc"><br>
-        <label>Telefone</label>
-        <input type="text" name="telefone"><br>
-        <label>ENDEREÇO</label>
-        <input type="text" name="endereco"><br>
+<h1>Novo cliente</h1>
+<form method="post" action="{{ route('clientes_novo') }}">
+    @csrf
+    <label>Nome</label>
+    <input type="text" name="nome"><br>
+    <label>CPF</label>
+    <input type="text" name="cpf"><br>
+    <label>RG</label>
+    <input type="text" name="rg"><br>
+    <label>Data de Nascimento</label>
+    <input type="date" name="data_nasc"><br>
+    <label>Telefone</label>
+    <input type="text" name="telefone"><br>
+    <label>ENDEREÇO</label>
+    <input type="text" name="endereco"><br>
 
-        <input type="submit" value="Enviar">
-    </form>
+    <input type="submit" value="Enviar">
+</form>
 
     @endsection
 </body>
