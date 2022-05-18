f5c61da153a576accc8d38f5c4054769c8b4e3d4
Luís Felippe Tomazini Fernandes <37600658+EutruTepit@users.noreply.github.com>
2022-05-18 07:03:39 -0300

    Update novo_produto.blade.php

--- resources/views/produto/novo_produto.blade.php
+++ resources/views/produto/novo_produto.blade.php
@@ -1,34 +1,21 @@
 @extends('template')
 
-
-@section('categoria')
-
-    <div class="categoria">
-        Test
-    </div>
-
-
-@endsection
 @section('conteudo')
-    <div class="borda_superior">
-        <h1>Hello</h1>
-    </div>
 
-<h1>Novo cliente</h1>
-<form action="{{route('produtos_salvar')}}" method="POST" enctype="multipart/form-data">
-    <h1>Novo produto</h1>
+<h1>Novo Produto</h1>
+<form action="{{ route('add_produto') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <label for="nome">Nome</label>
-        <input type="text" name="nome" id="nome"><br>
+        <input type="text" name="nome" id="nome" required><br>
 
         <label for="descricao">Descrição</label>
-        <input type="text" name="nome" id="descricao"><br>
+        <input type="text" name="nome" id="descricao" required><br>
 
         <label for="preco">Preço</label>
-        <input type="text" name="nome" id="preco"><br>
+        <input type="text" name="nome" id="preco" required><br>
 
         <label for="dataLancamento">Data de lançamento</label>
-        <input type="text" name="nome" id="dataLancamento"><br>
+        <input type="text" name="nome" id="dataLancamento" required><br>
 
         <!--<label for="arquivo">Arquivo</label>
         <input type="file" name="arquivo" id="arquivo">
