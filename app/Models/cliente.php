303feef58d844bdf8bc4d7cda977121f9b3cbca2
Luís Felippe Tomazini Fernandes <37600658+EutruTepit@users.noreply.github.com>
2022-05-18 08:27:23 -0300

    Relacionamento 1-1 cliente/user

--- app/Models/cliente.php
+++ app/Models/cliente.php
@@ -5,8 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class cliente extends Model
+class Cliente extends Model
 {
     use HasFactory;
     protected $table = "cliente";
+
+    function user(){
+        return $this->hasOne(User::class);
+    }
 }
