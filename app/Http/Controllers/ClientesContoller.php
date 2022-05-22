db2134ee8e84fe53a9d1405c2c2c7c89b1c1429a
Luís Felippe Tomazini Fernandes <37600658+EutruTepit@users.noreply.github.com>
2022-05-22 15:18:41 -0300

    Add viewPerfil, e delete(soft)

--- app/Http/Controllers/ClientesContoller.php
+++ app/Http/Controllers/ClientesContoller.php
@@ -13,4 +13,18 @@ class ClientesContoller extends Controller
 
         return view('cliente.editar_cliente', ['cliente' => $cliente]);
     }
+
+    function viewPerfilCliente(){
+        $cliente = Cliente::where('user_id', '=', Auth::id())->first();
+
+        return view('Cliente.perfil_cliente', ['cliente' => $cliente]);
+    }
+    
+    function deleteCliente(){
+        Cliente::where('user_id', '=', Auth::id())->first()->delete();
+
+        Auth::logout();
+        
+        return to_route('home');
+    }
 }
